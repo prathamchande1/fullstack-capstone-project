df0aab71fe1a4cd8eea311a73b6400b625c73cf0
prathamchande1 <prathamadsmn@gmail.com>
2026-09-09 02:27:09 -0400

    Code Update 1

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -5,8 +5,15 @@ const giftRoutes = require("./routes/giftRoutes");
 const authRoutes = require("./routes/authRoutes");
 const searchRoutes = require("./routes/searchRoutes");
 const app = express();
+const corsOptions = {
+    origin: "*",
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    optionsSuccessStatus: 204,
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/gifts", giftRoutes);

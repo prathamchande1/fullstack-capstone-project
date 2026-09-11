4a0735f088f9c6f837eb372464c3fce5fd273c46
prathamchande1 <prathamadsmn@gmail.com>
2026-09-11 04:19:32 -0400

    update

--- giftwebsite/index.js
+++ giftwebsite/index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
+const PORT = 9000;
+
+// Serve the React build folder
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// Serve home.html for the root route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'home.html'));
 });
 
-app.get('/app', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// Fallback for React/static pages
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'home.html'));
 });
 
-app.listen(9000);
+app.listen(PORT, () => {
+    console.log(`Gift Website running on port ${PORT}`);
+});
